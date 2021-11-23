73f69d3a03aafe6b0bb4ef319cb3a1dfe7c7d3f3
Jyothsna Boni <boni.jyothsna@gmail.com>
2021-11-23 21:13:52 +0100

    Include routing and navigation for card and to other components

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+/* import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
@@ -8,12 +8,18 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'card',
+        children: [
+          {
+          path: '',
+          loadChildren: () => import('../card/card.module').then(m => m.CardPageModule)
+         }
+       ]
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        /* if we want to undefine the particular tab, just change the tab2 with 'undefined'  in the code path:'tab2'  */
+        /* loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
         path: 'tab3',
@@ -35,5 +41,65 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule {}
+ */ 
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+/* import { CardPage } from '../card/card.page'; */
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'card',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../card/card.module').then(m => m.CardPageModule)
+            /* component: CardPage */
+          }
+        ]
+      },
+      {
+        path: 'tab2',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+          }
+        ]
+      },
+      {
+        path: 'tab3',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/card',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/card',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class TabsPageRoutingModule {}
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+/* import { CardPage } from '../card/card.page'; */
 
 @NgModule({
   imports: [
